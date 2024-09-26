e2438892e50eaf04106b1b61f4e283315b96f70e
brittytino <brittytino08@gmail.com>
2024-09-26 21:10:32 +0530

    ko

--- src/admin/pages/AIChatbot.tsx
+++ src/admin/pages/AIChatbot.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios'; // For making API requests
 import { useNavigate } from 'react-router-dom';
-import { PaperAirplaneIcon, ChipIcon } from '@heroicons/react/solid'; // Tailwind SVG icons
+import { PaperAirplaneIcon } from '@heroicons/react/outline';
 
 const AIChatbot: React.FC = () => {
   const [messages, setMessages] = useState<{ type: string; content: string }[]>([]);
